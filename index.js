b8a74c3583e85518744c21ed97f4e6c5763ffc45
NadirBakhsh <nadir4k2010@gmail.com>
2021-01-13 21:53:32 +0500

    express server listen

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hello World')
+})
+
+app.get('/users', (req, res) => {
+    res.send([
+        {
+            name: 'nadir'
+        },
+        {
+            name: 'khan'
+        },
+        {
+            name: 'Ali'
+        },
+    ])
+})
+
+app.listen(3000, () => {
+    console.log("list prot 3000")
+})
